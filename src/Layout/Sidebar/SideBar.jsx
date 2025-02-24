cbec9d12ee47e1254cfb9ea5057b00ee9241066e
tarun kumar ponnada <saiponnada46@gmail.com>
2025-02-24 11:19:16 +0530

    dashboard logo added

--- src/Layout/Sidebar/SideBar.jsx
+++ src/Layout/Sidebar/SideBar.jsx
@@ -49,7 +49,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
       >
         {/* Logo Image */}
         <img
-          src="."
+          src="../../public/swiftops.jpeg"
           alt="Logo"
           style={{
             width: "40px",
